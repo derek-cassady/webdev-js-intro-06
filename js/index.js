35aa8777b22ffb2d45abb0f21fdf96fb04ae2bee
derek-cassady <derek@cassadyweb.com>
2025-01-12 13:02:39 -0500

    experimented

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+//for loop with a break
+// for (let i = 1; i <= 10; i++) {
+//     // 'i' starts at 1, increases by one on each loop through, 
+//     // and runs as long as i <= 10.
+//     if (i === 5) {
+//         // If 'i' is equal to 5, triggers a break and .log.
+//         console.log("Breaking the loop at:", i);
+//         // Print a message.
+//         break; 
+//         // breaks the loop.
+//     }
+//     console.log("Current number:", i);
+//     // print the current number, outside of the IF condition
+// }
+
+//for loop with a continue
+for (let i = 1; i <= 10; i++) {
+    // 'i' starts at 1, increases by one on each loop through, 
+    // and runs as long as i <= 10.
+   
+    if (i === 5) {
+        // If 'i' is equal to 5, triggers the continue and .log.
+        console.log("Skipping number:", i);
+        // Print a message
+        continue; 
+        // Skip the rest of this loop and go to the next one.
+    }
+    console.log("Current number:", i);
+    // If 'i' is not 5, print current number.
+}
