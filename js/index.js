bc72b44ccaeec18370f26a47ce14d631e804397e
derek-cassady <derek@cassadyweb.com>
2025-01-12 13:05:07 -0500

    commented out index.js

--- js/index.js
+++ js/index.js
@@ -16,18 +16,18 @@
 //     // print the current number, outside of the IF condition
 // }
 
-//for loop with a continue
-for (let i = 1; i <= 10; i++) {
-    // 'i' starts at 1, increases by one on each loop through, 
-    // and runs as long as i <= 10.
+// //for loop with a continue
+// for (let i = 1; i <= 10; i++) {
+//     // 'i' starts at 1, increases by one on each loop through, 
+//     // and runs as long as i <= 10.
    
-    if (i === 5) {
-        // If 'i' is equal to 5, triggers the continue and .log.
-        console.log("Skipping number:", i);
-        // Print a message
-        continue; 
-        // Skip the rest of this loop and go to the next one.
-    }
-    console.log("Current number:", i);
-    // If 'i' is not 5, print current number.
-}
+//     if (i === 5) {
+//         // If 'i' is equal to 5, triggers the continue and .log.
+//         console.log("Skipping number:", i);
+//         // Print a message
+//         continue; 
+//         // Skip the rest of this loop and go to the next one.
+//     }
+//     console.log("Current number:", i);
+//     // If 'i' is not 5, print current number.
+// }
